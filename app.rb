488ce6fbbe4e0bced0bb724e49e9c44940f53026
Maxbey <mbeiner@crystalnix.com>
2018-01-18 13:53:05 +0600

    Improve Raven logging

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'dry-validation'
 require 'json'
+require 'securerandom'
 
 require_relative './boarding_service'
 
@@ -21,20 +22,33 @@ class BoardingApp < Sinatra::Base
       return validated.messages.to_json
     end
 
-    service = BoardingService.new
+    begin
+      service = BoardingService.new
 
-    unless service.get_tester(request_data['apple_id']).nil?
-      status 400
-      return {
-          :apple_id => ['Beta tester with that Apple ID already exists.']
-      }.to_json
+      unless service.get_tester(request_data['apple_id']).nil?
+        status 400
+        return {
+            :apple_id => ['Beta tester with that Apple ID already exists.']
+        }.to_json
+      end
+
+      service.add_tester(
+          request_data['apple_id'],
+          request_data['first_name'],
+          request_data['last_name']
+      )
+    rescue Exception => exception
+      status 424
+      Raven.capture_message(
+          "Unable to register #{request_data['apple_id']} in TestFlight",
+          :tags => {:apple_id => request_data['apple_id']},
+          :user => {:email => request_data['apple_id']},
+          :fingerprint => [SecureRandom.uuid.to_s],
+          :extra => {:exception => exception.message}
+      )
+
+      return
     end
-
-    service.add_tester(
-        request_data['apple_id'],
-        request_data['first_name'],
-        request_data['last_name']
-    )
 
     status 201
   end
